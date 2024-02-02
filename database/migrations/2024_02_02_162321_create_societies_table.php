12127a8100cfe230e5a2b08bd9331a9eb8b451ac
Suraj-14911 <124581811+Suraj-14911@users.noreply.github.com>
2024-02-02 22:53:51 +0530

    Update 2024_02_02_162321_create_societies_table.php

--- database/migrations/2024_02_02_162321_create_societies_table.php
+++ database/migrations/2024_02_02_162321_create_societies_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('societies', function (Blueprint $table) {
             $table->id();
+            $table->string('society_name');
+            $table->text('society_description');
             $table->timestamps();
+            
         });
     }
 
