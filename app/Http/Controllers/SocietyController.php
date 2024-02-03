11e53d7afe4e6fd9d7d9caabbacacc26cc10863c
Suraj-14911 <124581811+Suraj-14911@users.noreply.github.com>
2024-02-03 20:34:23 +0530

    Update SocietyController.php

--- app/Http/Controllers/SocietyController.php
+++ app/Http/Controllers/SocietyController.php
@@ -1,10 +1,62 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
+use App\Models\Society;
 
 class SocietyController extends Controller
 {
-    //
+     
+
+    public function insert(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:100',
+            'logo' => 'required|mimes:jpg,png|max:2048', 
+            'description' => 'required|string',
+        ]);
+
+        if ($request->fails()) {
+            return response()->json(['message' => 'Validation failed', 'errors' => $request->errors()], 400);
+        }
+        $created = Society::create($request->all());
+
+
+        if ($created) {
+            return response()->json(['message' => 'Record created successfully'], 201);
+        } else {
+            return response()->json(['message' => 'Failed to create record'], 500);
+        }
+    }
+    
+    public function update(Request $request, $id):JsonResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'logo' => 'nullable|mimes:jpg,png|max:2048', 
+            'description' => 'required|string',
+        ]);
+
+        if ($request->fails()) {
+            return response()->json(['message' => 'Validation failed', 'errors' => $request->errors()], 400);
+        }
+
+        // Find the record by its ID
+        $record = Society::find($id);
+
+      
+        if ($record) {
+           
+            $record->update($request->all());
+
+            return response()->json(['message' => 'Record updated successfully'], 200);
+        } else {
+            return response()->json(['message' => 'Record not found'], 404);
+        }   
+
+
+    
+}
 }
