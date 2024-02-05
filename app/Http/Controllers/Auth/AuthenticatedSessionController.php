56c3f651146a71790e3f955ad334612177ec7c71
Rishi <rk3141508@gmail.com>
2024-02-05 21:06:47 +0530

    jwt created

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,37 +2,93 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\GeneralResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Handle an incoming authentication request.
-     */
-    public function store(LoginRequest $request): Response
+
+    public function __construct()
     {
-        $request->authenticate();
+        $this->middleware('auth:api', ['except' => ['store']]);
+    }
 
-        $request->session()->regenerate();
+    public function checkCredentials(Request $request)
+    {
+        $request->only(['email', 'password']);
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'error' => ['Email and password does not match our records.']
+            ]);
+        }
 
-        return response()->noContent();
+        return $user;
     }
 
     /**
-     * Destroy an authenticated session.
+     * Handle an incoming authentication request.
      */
-    public function destroy(Request $request): Response
+    public function store(Request $request): JsonResponse
     {
-        Auth::guard('web')->logout();
+        $this->checkCredentials($request);
+        $credentials = $request->only(['email', 'password']);
+        $token = auth()->attempt($credentials);
+        if (!$token) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return $this->respondWithToken($token);
+    }
 
-        $request->session()->invalidate();
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResource
+     */
+    public function me(): JsonResource
+    {
+        return new GeneralResource(auth()->user());
+    }
 
-        $request->session()->regenerateToken();
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh(): JsonResponse
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
 
-        return response()->noContent();
+    protected function respondWithToken($token): JsonResponse
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(): JsonResponse
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,9 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -52,5 +53,25 @@ class User extends Authenticatable
     public function competitions(): BelongsToMany
     {
         return $this->belongsToMany(Competition::class);
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
