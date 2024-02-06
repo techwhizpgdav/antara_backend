dc50957717bf70fd4c26402be17e31a56bf4ff9f
Rishi <rk3141508@gmail.com>
2024-02-06 22:41:28 +0530

    test done

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return ['Laravel' => app()->version()];
+    return ['Laravel' => app()->version()];
 });
 
 Route::post('test', [ParticipationController::class, 'store']);
