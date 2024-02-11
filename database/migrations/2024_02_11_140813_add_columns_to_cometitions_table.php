9a8985952a1be15abcef1851554c705f8af3feba
Rishi <rk3141508@gmail.com>
2024-02-11 20:17:34 +0530

    pull

---
+++ database/migrations/2024_02_11_140813_add_columns_to_cometitions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('competitions', function (Blueprint $table) {
+            $table->date('date')->nullable()->after('rules');
+            $table->time('start_at')->after('date');
+            $table->time('ends_at')->after('start_at');
+            $table->string('venue')->after('ends_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('competitions', function (Blueprint $table) {
+            //
+        });
+    }
+};
