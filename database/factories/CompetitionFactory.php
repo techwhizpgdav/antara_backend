2c00f27f233c96b58617d6692262bf0e87305719
Suraj-14911 <124581811+Suraj-14911@users.noreply.github.com>
2024-02-12 19:12:26 +0530

    timeline

--- database/factories/CompetitionFactory.php
+++ database/factories/CompetitionFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Doctrine\Inflector\Rules\Word;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,10 +19,19 @@ class CompetitionFactory extends Factory
     {
         return [
             //
-            'categor_id' => $this->faker->numberbetween(1,10),
+            'category_id' => $this->faker->numberbetween(1,10),
             'society_id' => $this->faker->numberbetween(1,10),
             'title' => $this->faker->name(),
-            'logo' => $this->faker->url(),
+            'venue' => $this->faker->word(),
+            'description' =>$this->faker->sentence(),
+            'minimum_size' =>$this->faker->numberbetween(0,1),
+            'maximum_size' =>$this->faker->numberbetween(0,1),
+            'image_url' =>$this->faker->url(),
+            'date'=>$this->faker->date(),
+            'start_at'=> $this->faker->time(),
+            'ends_at'=> $this->faker->time(),
+
+            
         ];
     }
 }
