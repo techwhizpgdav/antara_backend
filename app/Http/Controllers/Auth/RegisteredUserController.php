7d65325539f4566945e04fac69b713268dc93fca
Rishi <rk3141508@gmail.com>
2024-02-18 21:42:53 +0530

    cors

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,6 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'email_verified_at' => now()
         ]);
         // ->assignRole('user');
 
