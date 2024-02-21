789ebf26871d09bc1f539113aae0d946eff37108
Rishi <rk3141508@gmail.com>
2024-02-21 15:17:14 +0530

    cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,6 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Support\Facades\Artisan;
 
 class Kernel extends ConsoleKernel
 {
@@ -14,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call(Artisan::call('queue:work --stop-when-empty'))->everyMinute();
+        $schedule->call('queue:work --stop-when-empty')->everyMinute();
     }
 
     /**
