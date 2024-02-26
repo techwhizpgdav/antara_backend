a500e2437fc3baa32c998bc09b44a67caf61c1b4
Rishi <rk3141508@gmail.com>
2024-02-26 19:21:11 +0530

    soft del

--- app/Models/Competition.php
+++ app/Models/Competition.php
@@ -12,10 +12,11 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Competition extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'category_id',
--- app/Models/Society.php
+++ app/Models/Society.php
@@ -6,10 +6,11 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Society extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $tables = 'societies';
     protected $fillable = [
         'name',
